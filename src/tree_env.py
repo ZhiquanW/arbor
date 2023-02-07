1a36c0d5529beb189ddb977aa61a0b9ddc63215a
ZhiquanW <zhiquan.wzq@gmail.com>
2023-02-07 16:42:04 -0500

    [dawn] *debug: tree branch angle

--- src/tree_env.py
+++ src/tree_env.py
@@ -9,6 +9,7 @@ from rlvortex.envs.base_env import BaseEnvTrait, EnvWrapper
 
 import utils
 import os
+import random
 
 
 class PolyLineTreeEnv(BaseEnvTrait):
@@ -20,7 +21,8 @@ class PolyLineTreeEnv(BaseEnvTrait):
         init_dis: float = 0.5,
         delta_dis_range: np.ndarray[int, np.dtype[np.float64]],
         delta_rotate_range: np.ndarray[int, np.dtype[np.float64]],
-        new_branch_rot_range: np.ndarray[int, np.dtype[np.float64]],
+        init_branch_rot: float,
+        branch_rot_range: np.ndarray[int, np.dtype[np.float64]],
         branch_prob_range: np.ndarray[int, np.dtype[np.float64]],
         sleep_prob_range: np.ndarray[int, np.dtype[np.float64]],
         matplot: bool = True,
@@ -39,6 +41,8 @@ class PolyLineTreeEnv(BaseEnvTrait):
         assert len(delta_dis_range) == 2
         assert np.all(self.init_dis + delta_dis_range > 0)
         self.delta_dis_range = delta_dis_range
+        assert init_branch_rot > 0 and init_branch_rot < 180
+        self.init_branch_rot = init_branch_rot
         assert len(delta_rotate_range) == 2
         assert delta_rotate_range[0] >= -180 and delta_rotate_range[1] <= 180
         self.delta_rotate_range = delta_rotate_range  # rotate around direction with angle in degreee
@@ -48,7 +52,7 @@ class PolyLineTreeEnv(BaseEnvTrait):
         self.sleep_prob_range = sleep_prob_range
         # the angle between the new branch and the parent branch,
         # the init rotation of the new node is compute by rotating alpha degree around parent node x axis ([1,0,0] in local frame)
-        self.new_branch_rot_range = new_branch_rot_range
+        self.branch_rot_range = branch_rot_range
         # buds on the tree
         self.steps = 0
         # render variables
@@ -150,6 +154,8 @@ class PolyLineTreeEnv(BaseEnvTrait):
             list(filter(lambda row_idx: row_idx < self.num_bud, np.where(self.buds_states_h[:, -1] == 0)[0]))
         ).astype(np.int32)
         num_awake_buds = awake_bud_indices.shape[0]
+
+        # 3. awake buds perform actions
         if num_awake_buds > 0:
             # check and extract different actions
             act_delta_move_dis_g = action_2d[awake_bud_indices, 0].reshape(-1, 1)
@@ -170,11 +176,6 @@ class PolyLineTreeEnv(BaseEnvTrait):
             assert np.all(branch_probs_g >= 0.0) and np.all(
                 branch_probs_g <= 1.0
             ), "branch_prob should be in the range of [0, 1]"
-            # check the sleep prob is normalized to [0,1]
-            sleep_probs_g = utils.unscale_by_range(
-                action_2d[awake_bud_indices, 5], self.sleep_prob_range[0], self.sleep_prob_range[1]
-            )
-            assert np.all(sleep_probs_g >= 0.0) and np.all(sleep_probs_g <= 1.0), "sleep_prob should be in the range of [0, 1]"
 
             # 1. rotate the direction of the buds
             # 1.1 get delta rotation matrix, unscale to [-180, 180] in degrees
@@ -207,8 +208,17 @@ class PolyLineTreeEnv(BaseEnvTrait):
             self.buds_states_h[awake_bud_indices, 1:4] += move_dis_h * curr_up_dirs
             # 2.2 update the rotation
             self.buds_states_h[awake_bud_indices, 4:7] = utils.scale_by_range(curr_euler_degree, -180, 180)
+            # 3. set buds to sleep
+            # the num_awake_buds is the number of buds that are awake before growing new branches
+            # check the sleep prob is normalized to [0,1]
+            sleep_probs_g = utils.unscale_by_range(
+                action_2d[awake_bud_indices, 5], self.sleep_prob_range[0], self.sleep_prob_range[1]
+            )
+            assert np.all(sleep_probs_g >= 0.0) and np.all(sleep_probs_g <= 1.0), "sleep_prob should be in the range of [0, 1]"
+            sleep_indices = awake_bud_indices[np.where(np.random.uniform(0, 1, num_awake_buds) < sleep_probs_g)[0]]
+            self.buds_states_h[sleep_indices, 7] = 1  # set selected buds to sleep
 
-            # 2.3 grow new branch by sampling the prob
+            # 4. grow new branch by sampling the prob
             # the indices of the buds that will grow new branches  from awake buds
             grow_indices = awake_bud_indices[np.where(np.random.uniform(0, 1, num_awake_buds) < branch_probs_g)[0]]
             # print(f"{}: {}"., self.steps, len(grow_indices))
@@ -238,9 +248,13 @@ class PolyLineTreeEnv(BaseEnvTrait):
                     num_child_buds,
                     3,
                 ), f"rot_axes.shape={rot_axes.shape} does not match ({num_child_buds}, 3)"
-                new_branch_rot_angle = np.random.uniform(
-                    self.new_branch_rot_range[0], self.new_branch_rot_range[1], size=num_child_buds
-                ).reshape(num_child_buds, -1)
+                new_branch_rot_angle = random.choice([1, -1]) * (
+                    self.init_branch_rot
+                    + np.random.uniform(self.branch_rot_range[0], self.branch_rot_range[1], size=num_child_buds).reshape(
+                        num_child_buds, -1
+                    )
+                )
+                print(new_branch_rot_angle)
                 new_branch_rot_mat = utils.rot_mat_from_axis_angles(rot_axes, new_branch_rot_angle)
                 assert new_branch_rot_angle.shape == (
                     num_child_buds,
@@ -262,24 +276,16 @@ class PolyLineTreeEnv(BaseEnvTrait):
                 new_branch_euler_degree = (new_branch_euler_degree % 360 + 180) % 360 - 180
                 self.buds_states_h[child_buds_indices, 4:7] = utils.scale_by_range(new_branch_euler_degree, -180, 180)
                 self.buds_born_step_hist[child_buds_indices] = self.steps
-            # 3. set buds to sleep
-            # the num_awake_buds is the number of buds that are awake before growing new branches
-            sleep_indices = awake_bud_indices[np.where(np.random.uniform(0, 1, num_awake_buds) < sleep_probs_g)[0]]
-            self.buds_states_h[sleep_indices, 7] = 1  # set selected buds to sleep
-        # 4. record information into history
+
+        # 5. record information into history
         self.buds_states_hist[self.steps, : self.num_bud] = self.buds_states_h[: self.num_bud]
 
         # Compute Rewards Functions
-        # 1. compute the height reward by summizeing
+        # 6. compute the height reward by summizeing
         buds_height = self.buds_states_h[: self.num_bud, 2]
         r_height = self.w_height * np.mean(buds_height)
         reward = r_height
-        done = False
-        if num_awake_buds == 0:
-            done = True
-        if any(buds_height < 0):
-            done = True
-            reward -= 100
+        done = num_awake_buds == 0 or any(buds_height < 0)
         if not self.renderable:
             self.render()
         return self.buds_states_h.flatten(), reward, done, {"num_buds": self.num_bud, "mean_buds_height": np.mean(buds_height)}
@@ -299,9 +305,9 @@ class PolyLineTreeEnv(BaseEnvTrait):
 
         for v_idx in range(self.num_bud):
             born_step = int(self.buds_born_step_hist[v_idx])
-            points_x = self.buds_states_hist[born_step : self.steps, v_idx, 1].squeeze()
-            points_y = self.buds_states_hist[born_step : self.steps, v_idx, 2].squeeze()
-            points_z = self.buds_states_hist[born_step : self.steps, v_idx, 3].squeeze()
+            points_x = self.buds_states_hist[born_step : self.steps + 1, v_idx, 1].squeeze()
+            points_y = self.buds_states_hist[born_step : self.steps + 1, v_idx, 2].squeeze()
+            points_z = self.buds_states_hist[born_step : self.steps + 1, v_idx, 3].squeeze()
             self.ax1.plot(points_x, points_z, points_y)
             self.ax1.set_aspect("equal", adjustable="box")
             z_limit = np.max(np.abs(points_y)) * 1.1
